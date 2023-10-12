4c6d9a9681783363e3cd6cd8ce64d6a19593a185
김경렬 <rlarudfuf802@naver.com>
2023-10-12 23:44:30 +0900

    다대다 [N:N]

---
+++ src/main/java/relationmapping/MemberProduct.java
@@ -0,0 +1,28 @@
+package relationmapping;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * 다대다 관계를 일대다, 다대일 관계로 풀어내기 위한 엔티티
+ * (사실상 해당 엔티티는 Order와 같은 느낌의 엔티티다.)
+ */
+@Entity
+public class MemberProduct {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "PRODUCT_ID")
+    private Product product;
+
+    private int count;
+    private int price;
+    private LocalDateTime orderDateTime;
+}
