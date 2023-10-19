0ae12a8d6591290cc0ea02e6b6fcc6a5eb79c37c
김경렬 <rlarudfuf802@naver.com>
2023-10-19 23:32:09 +0900

    실전 예제4 - 상속관계 매핑

--- src/main/java/jpabook/jpashop/domain/Movie.java
+++ src/main/java/jpabook/jpashop/domain/Movie.java
@@ -1,10 +1,8 @@
-package relationmapping;
+package jpabook.jpashop.domain;
 
-import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
-//@Entity
-@DiscriminatorValue("M")
+@Entity
 public class Movie extends Item {
 
     private String director;
