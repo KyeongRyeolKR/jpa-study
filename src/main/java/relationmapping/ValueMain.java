b2cd42ec734a73d768656592ce4e18c353826cea
김경렬 <rlarudfuf802@naver.com>
2023-10-21 18:57:50 +0900

    값 타입의 비교

--- src/main/java/relationmapping/ValueMain.java
+++ src/main/java/relationmapping/ValueMain.java
@@ -42,14 +42,22 @@ package relationmapping;
  *   - 새로 객체를 만들어서 생성자에 바꾸고 싶은 값으로 넣어서 대입해야한다.
  *
  *   정리 : 깔끔하게 모든 값 타입은 불변 객체로 만들어야한다!!!!
+ *
+ * - 값 타입의 비교
+ *   - 동일성(identity) 비교 : 인스턴스의 참조 값을 비교, == 사용
+ *   - 동등성(equivalence) 비교 : 인스턴스의 값을 비교, equals() 사용
+ *   - 값 타입(임베디드/객체 타입)의 비교는 항상 equals()를 사용해서 비교해야한다.
  */
 public class ValueMain {
 
     public static void main(String[] args) {
-        Integer a = 10;
-        Integer b = a;
+        int a = 10;
+        int b = 10;
+        System.out.println("a == b : " + (a == b));
 
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        Address address1 = new Address("city", "street", "100");
+        Address address2 = new Address("city", "street", "100");
+        System.out.println("address1 == address2 : " + (address1 == address2));
+        System.out.println("address1 equals address2 : " + (address1.equals(address2)));
     }
 }
