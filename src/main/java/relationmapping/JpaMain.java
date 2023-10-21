30d78c1a165c663cd34695b2b653b57b44f8dc08
김경렬 <rlarudfuf802@naver.com>
2023-10-21 18:49:56 +0900

    값 타입의 비교

--- src/main/java/relationmapping/JpaMain.java
+++ src/main/java/relationmapping/JpaMain.java
@@ -94,12 +94,15 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("hello");
-            member.setHomeAddress(new Address("city", "street", "100"));
-            member.setWorkPeriod(new Period());
+            Address address = new Address("city", "street", "100");
 
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            member1.setHomeAddress(address);
+            em.persist(member1);
+
+            Address newAddress = new Address("newCity", address.getStreet(), address.getZipcode());
+            member1.setHomeAddress(newAddress);
 
             tx.commit();
         } catch (Exception e) {
