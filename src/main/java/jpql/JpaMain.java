f38ce333a74fc73124d8e29dc4b3a7f1708b1c3a
김경렬 <rlarudfuf802@naver.com>
2023-10-24 00:47:09 +0900

    페치 조인2 - 한계

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -169,6 +169,24 @@ import java.util.List;
  *   - 단지 SELECT 절에 지정한 엔티티만 조회할 뿐
  *   - 페치 조인을 사용할 때만 연관된 엔티티도 함께 조회함(즉시 로딩)
  *   - 페치 조인은 객체 그래프를 SQL 한번에 조회하는 개념이다!
+ *
+ *   페치 조인의 특징과 한계
+ *   - 페치 조인 대상에는 별칭을 사용할 수 없다.
+ *     - 하이버네이트는 가능, 가급적 사용하면 안됨
+ *   - 둘 이상의 컬렉션은 페치 조인할 수 없다.
+ *   - 컬렉션을 페치 조인하면 페이징 API를 사용할 수 없다.
+ *     - 일대일, 다대일 같은 단일 값 연관 필드들은 페치 조인해도 페이징 가능
+ *     - 하이버네이트는 경고 로그를 남기고 메모리에서 페이징(매우 위험)
+ *   - 연관된 엔티티들은 SQL 한번으로 조회 - 성능 최적화
+ *   - 엔티티에 직접 적용하는 글로벌 로딩 전략보다 우선됨
+ *     - 글로벌 로딩 전략 : @OneToMany(fetch = FetchType.LAZY)
+ *   - 최적화가 필요한 곳은 페치 조인 적용하면 대부분의 N + 1 문제가 해결됨
+ *
+ *   정리
+ *   - 모든 것을 페치 조인으로 해결할 수는 없음
+ *   - 페치 조인은 객체 그래프를 유지할 때 사용하면 효과적(객체 그래프 탐색)
+ *   - 여러 테이블에서 조인해서 엔티티가 가진 모양이 아닌 전혀 다른 결과를 내야 한다면,
+ *     페치 조인보다는 일반 조인을 사용하고 필요한 데이터들만 조회해서 DTO로 반환하는 것이 효과적이다!
  */
 public class JpaMain {
 
@@ -208,8 +226,10 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select t from Team t join fetch t.members m";
+            String query = "select t from Team t";
             List<Team> result = em.createQuery(query, Team.class)
+                    .setFirstResult(0)
+                    .setMaxResults(2)
                     .getResultList();
 
             for (Team team : result) {
